572451df0048753c470bb8ab58d3191a51662ae0
ngthanhvu <nguyenthanhvudvfb@gmail.com>
2025-10-17 22:19:30 +0700

    feat: enhance theme support by updating meta tags and adding dark/light mode handling in App.vue; fix CSS formatting

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,25 @@
 </template>
 
 <script lang="ts" setup>
+import { watchEffect } from 'vue'
 import { Notivue, Notification } from 'notivue'
+
+watchEffect(() => {
+  const theme = document.documentElement.getAttribute('data-theme')
+  const metaThemeColor = document.querySelector('#meta-theme-color')
+  const metaStatusBar = document.querySelector('#meta-status-bar')
+
+  if (theme === 'dark') {
+    metaThemeColor?.setAttribute('content', '#0f172a') // màu dark background
+    metaStatusBar?.setAttribute('content', 'black') // status bar chữ trắng
+    document.body.style.backgroundColor = '#0f172a'
+  } else {
+    metaThemeColor?.setAttribute('content', '#ffffff') // màu light background
+    metaStatusBar?.setAttribute('content', 'default')
+    document.body.style.backgroundColor = '#ffffff'
+  }
+})
+
 </script>
 
 <style lang="scss" scoped></style>
